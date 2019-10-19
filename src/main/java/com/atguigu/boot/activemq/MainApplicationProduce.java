e5da0530d0b68d01f8a0f7a206080df7945f5a99
yanlp <85827444@qq.com>
2019-10-19 23:51:59 +0800

    SpringBoot与ActiveMQ整合之 队列生产者  间隔定投

--- src/main/java/com/atguigu/boot/activemq/MainApplicationProduce.java
+++ src/main/java/com/atguigu/boot/activemq/MainApplicationProduce.java
@@ -2,6 +2,8 @@ package com.atguigu.boot.activemq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 /**
  * 主启动类
@@ -12,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version 1.0
  */
 @SpringBootApplication
+@EnableScheduling
 public class MainApplicationProduce {
     public static void main(String[] args) {
         SpringApplication.run(MainApplicationProduce.class,args);
--- src/main/java/com/atguigu/boot/activemq/produce/QueueProduce.java
+++ src/main/java/com/atguigu/boot/activemq/produce/QueueProduce.java
@@ -2,6 +2,7 @@ package com.atguigu.boot.activemq.produce;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.jms.Queue;
@@ -22,8 +23,20 @@ public class QueueProduce {
     @Autowired
     private Queue queue;
 
+    /**
+     * 正常生产消息
+     */
     public void produceMsg() {
         jmsMessagingTemplate.convertAndSend(queue,"*******: " + UUID.randomUUID().toString().substring(0,6));
+    }
+
+    /**
+     * 每隔3秒钟生产一条消息
+     */
+    @Scheduled(fixedDelay = 3000)
+    public void produceMsgScheduled() {
+        jmsMessagingTemplate.convertAndSend(queue,"******* produceMsgScheduled: " + UUID.randomUUID().toString().substring(0,6));
+        System.out.println("*********produceMsgScheduled  send ok");
     }
 
 
